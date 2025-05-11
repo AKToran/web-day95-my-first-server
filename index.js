2dcba6a0d5d5ce3d966bccc5b807f8a3e4065dcd
AKToran <aktoran60@gmail.com>
2025-05-11 20:24:50 +0600

    nodemon index.js - monitors changes and updates them

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res)=>{
   res.send('Hello from my first express server.');
 })
 
+app.get('/data', (req, res)=>{
+  res.send("More data loading soon...");
+})
+
+app.get('/auth', (req, res)=>{
+  res.send("authentication....")
+})
+
 app.listen(port, () =>{
   console.log(`My first server is running on port: ${port}`);
 })

596e61d7f40a6b9ab13491ff3332c335e0c63d3e
AKToran <aktoran60@gmail.com>
2025-05-12 00:38:36 +0600

    send json data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express();
+const phones = require('./phones.json');
+const app = express(); 
 const port = 5000;
 
 app.get('/', (req, res)=>{
@@ -12,6 +13,17 @@ app.get('/data', (req, res)=>{
 
 app.get('/auth', (req, res)=>{
   res.send("authentication....")
+})
+
+app.get('/phones', (req, res)=>{
+  res.send(phones);
+})
+
+app.get('/phones/:id', (req, res)=>{
+  const id = parseInt(req.params.id);
+  console.log('data for id', id);
+  const phone = phones.find(phone => phone.id === id) || {};
+  res.send(phone)
 })
 
 app.listen(port, () =>{
